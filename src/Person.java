4f77cb8b8582923234b4e911f828d71b743c6c13
Shivang Brahmbhatt <shivangbarot27@gmail.com>
2024-09-25 15:41:44 -0400

    first initlize the branch - commited basic code -created test file

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    String Person(String firstName, String lastName)
+    {
+        this.firstName = " ";
+        this.lastName = " ";
+
+    }
+}
